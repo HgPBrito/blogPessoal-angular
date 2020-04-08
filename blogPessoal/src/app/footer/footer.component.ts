c6b4aef28430b5ab2c1d2afe8c22437e30e2d96e
Hugo Brito <hgbrito.tr@gmail.com>
2020-04-08 15:01:05 -0300

    Instalação do Bootstrap, implementação da navbar e footer

--- blogPessoal/src/app/footer/footer.component.ts
+++ blogPessoal/src/app/footer/footer.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { faInstagramSquare } from '@fortawesome/free-brands-svg-icons';
+import { faFacebookSquare } from '@fortawesome/free-brands-svg-icons';
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
 @Component({
   selector: 'app-footer',
@@ -6,6 +9,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.css']
 })
 export class FooterComponent implements OnInit {
+
+  faInstagramSquare = faInstagramSquare;
+  faFacebookSquare = faFacebookSquare;
+  faLinkedin = faLinkedin;
 
   constructor() { }
 
